e7ffcc0c0b96854731eb5ff884697428a5741a21
sudo-m3zim <nizilplay2004@gmail.com>
2024-04-03 23:34:37 +0300

    Added texture loading

--- VulcanTest/main.cpp
+++ VulcanTest/main.cpp
@@ -2,12 +2,14 @@
 #define GLFW_EXPOSE_NATIVE_WIN32
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#define STB_IMAGE_IMPLEMENTATION
 
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 #include <vulkan/vulkan.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <stb_image.h>
 
 #include <stdexcept>
 #include <iostream>
@@ -116,6 +118,9 @@ private:
     vector<VkDeviceMemory> uniform_buffers_memory;
     vector<void*> uniform_buffers_mapped;
 
+    VkImage texture_image;
+    VkDeviceMemory texture_image_memory;
+
     vector<VkSemaphore> image_semaphores;
     vector<VkSemaphore> render_semaphores;
     vector<VkFence> in_flight_fences;
@@ -159,6 +164,14 @@ private:
     uint32_t get_graphics_family_index();
     uint32_t get_present_family_index();
     uint32_t get_memory_type(uint32_t filter, VkMemoryPropertyFlags properties);
+
+    VkCommandBuffer begin_single_time_commands();
+    void end_single_time_commands(VkCommandBuffer command_buff);
+    void add_texture_image();
+    void add_image(uint32_t texture_width, uint32_t texture_height, VkFormat format, VkImageTiling tiling,
+        VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& image_memory);
+    void change_image_layout(VkImage image, VkFormat format, VkImageLayout layout, VkImageLayout new_layout);
+    void copy_buffer_to_image(VkBuffer buff, VkImage image, uint32_t width, uint32_t height);
     
     void record_command_buffer(VkCommandBuffer buff, uint32_t image_index);
     void draw_frame();
@@ -181,6 +194,7 @@ void VulkanManager::start_vulkan()
     add_graphics_pipeline();
     add_framebuffers();
     add_command_pool();
+    add_texture_image();
     add_vertex_buffer();
     add_indices_buffer();
     add_uniform_buffers();
@@ -531,39 +545,15 @@ void VulkanManager::add_buffer(VkBuffer& buff, VkDeviceMemory& buff_memory, VkDe
 
 void VulkanManager::copy_buffer(VkBuffer from_buff, VkBuffer to_buff, VkDeviceSize size)
 {
-    VkQueue graphics_queue;
-    VkCommandBufferAllocateInfo command_buffer_allocate_info{};
-    VkCommandBufferBeginInfo begin_info{};
-    VkCommandBuffer command_buff;
     VkBufferCopy copy_region{};
-    VkSubmitInfo submit_info{};
-
-    vkGetDeviceQueue(logical_device, get_graphics_family_index(), 0, &graphics_queue);
-
-    command_buffer_allocate_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-    command_buffer_allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-    command_buffer_allocate_info.commandPool = command_pool;
-    command_buffer_allocate_info.commandBufferCount = 1;
-    
-    begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-    begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    VkCommandBuffer command_buff = begin_single_time_commands();
 
     copy_region.srcOffset = 0;
     copy_region.dstOffset = 0;
     copy_region.size = size;
 
-    submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-    submit_info.commandBufferCount = 1;
-    submit_info.pCommandBuffers = &command_buff;
-
-    vkAllocateCommandBuffers(logical_device, &command_buffer_allocate_info, &command_buff);
-    vkBeginCommandBuffer(command_buff, &begin_info);
     vkCmdCopyBuffer(command_buff, from_buff, to_buff, 1, &copy_region);
-    vkEndCommandBuffer(command_buff);
-    vkQueueSubmit(graphics_queue, 1, &submit_info, VK_NULL_HANDLE);
-    vkQueueWaitIdle(graphics_queue);
-
-    vkFreeCommandBuffers(logical_device, command_pool, 1, &command_buff);
+    end_single_time_commands(command_buff);
 }
 
 void VulkanManager::add_vertex_buffer()
@@ -956,6 +946,132 @@ void VulkanManager::add_framebuffers()
     }
 }
 
+void VulkanManager::add_image(uint32_t texture_width, uint32_t texture_height, VkFormat format, VkImageTiling tiling,
+    VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& image_memory)
+{
+    VkImageCreateInfo image_create_info{};
+    VkMemoryRequirements memory_requirements{};
+    VkMemoryAllocateInfo texture_image_memory_alloc_info{};
+
+    image_create_info.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    image_create_info.imageType = VK_IMAGE_TYPE_2D;
+    image_create_info.extent.width = texture_width;
+    image_create_info.extent.height = texture_height;
+    image_create_info.extent.depth = 1;
+    image_create_info.mipLevels = 1;
+    image_create_info.arrayLayers = 1;
+    image_create_info.format = format;
+    image_create_info.tiling = tiling;
+    image_create_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    image_create_info.usage = usage;
+    image_create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    image_create_info.samples = VK_SAMPLE_COUNT_1_BIT;
+
+    if (vkCreateImage(logical_device, &image_create_info, nullptr, &image) != VK_SUCCESS)
+        cout << "Adding image error!" << endl;
+
+    vkGetImageMemoryRequirements(logical_device, image, &memory_requirements);
+    texture_image_memory_alloc_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    texture_image_memory_alloc_info.allocationSize = memory_requirements.size;
+    texture_image_memory_alloc_info.memoryTypeIndex = get_memory_type(memory_requirements.memoryTypeBits, properties);
+
+    if (vkAllocateMemory(logical_device, &texture_image_memory_alloc_info, nullptr, &image_memory) != VK_SUCCESS)
+        cout << "Allocating image memory error!" << endl;
+
+    vkBindImageMemory(logical_device, image, image_memory, 0);
+}
+
+void VulkanManager::add_texture_image()
+{
+    int image_width, image_height, image_channels;
+    VkDeviceSize image_size;
+    stbi_uc* image_pixels = stbi_load("Textures/Gabe.jpg", 
+        &image_width, &image_height, &image_channels, STBI_rgb_alpha);
+    VkBuffer staging_buffer;
+    VkDeviceMemory staging_buffer_memory;
+    void* data;
+
+    image_size = image_width * image_height * 4;
+
+    add_buffer(staging_buffer, staging_buffer_memory, image_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    vkMapMemory(logical_device, staging_buffer_memory, 0, image_size, 0, &data);
+    memcpy(data, image_pixels, static_cast<size_t>(image_size));
+    vkUnmapMemory(logical_device, staging_buffer_memory);
+
+    stbi_image_free(image_pixels);
+
+    add_image(image_width, image_height, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
+        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+        texture_image, texture_image_memory);
+    change_image_layout(texture_image, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+    copy_buffer_to_image(staging_buffer, texture_image, static_cast<uint32_t>(image_width), static_cast<uint32_t>(image_height));
+    change_image_layout(texture_image, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+    vkDestroyBuffer(logical_device, staging_buffer, nullptr);
+    vkFreeMemory(logical_device, staging_buffer_memory, nullptr);
+}
+
+void VulkanManager::change_image_layout(VkImage image, VkFormat format, VkImageLayout layout, VkImageLayout new_layout)
+{
+    VkCommandBuffer command_buff = begin_single_time_commands();
+    VkImageMemoryBarrier barrier{};
+    VkPipelineStageFlags source_stage;
+    VkPipelineStageFlags destination_stage;
+
+    barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    barrier.oldLayout = layout;
+    barrier.newLayout = new_layout;
+    barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.image = image;
+    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    barrier.subresourceRange.baseMipLevel = 0;
+    barrier.subresourceRange.levelCount = 1;
+    barrier.subresourceRange.baseArrayLayer = 0;
+    barrier.subresourceRange.layerCount = 1;
+
+    if (layout == VK_IMAGE_LAYOUT_UNDEFINED and new_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+    {
+        barrier.srcAccessMask = 0;
+        barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        source_stage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        destination_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+    }
+
+    else if(layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL and new_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+    {
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        source_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        destination_stage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    }
+
+    vkCmdPipelineBarrier(command_buff, source_stage, destination_stage, 0, 0, nullptr, 0, nullptr, 1, &barrier);
+    end_single_time_commands(command_buff);
+}
+
+void VulkanManager::copy_buffer_to_image(VkBuffer buff, VkImage image, uint32_t width, uint32_t height)
+{
+    VkBufferImageCopy region{};
+    VkCommandBuffer command_buff = begin_single_time_commands();
+
+    region.bufferOffset = 0;
+    region.bufferRowLength = 0;
+    region.bufferImageHeight = 0;
+    region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    region.imageSubresource.mipLevel = 0;
+    region.imageSubresource.baseArrayLayer = 0;
+    region.imageSubresource.layerCount = 1;
+
+    region.imageOffset = { 0, 0, 0 };
+    region.imageExtent = { width, height, 1 };
+
+    vkCmdCopyBufferToImage(command_buff, buff, image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
+    end_single_time_commands(command_buff);
+}
+
 void VulkanManager::add_command_pool()
 {
     VkCommandPoolCreateInfo command_pool_create_info{};
@@ -971,6 +1087,44 @@ void VulkanManager::add_command_pool()
         return;
     }
     cout << "Creating command pool success!" << endl;
+}
+
+VkCommandBuffer VulkanManager::begin_single_time_commands()
+{
+    VkCommandBufferAllocateInfo command_buffer_allocate_info{};
+    VkCommandBufferBeginInfo begin_info{};
+    VkCommandBuffer command_buff;
+
+    command_buffer_allocate_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    command_buffer_allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    command_buffer_allocate_info.commandPool = command_pool;
+    command_buffer_allocate_info.commandBufferCount = 1;
+
+    begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    vkAllocateCommandBuffers(logical_device, &command_buffer_allocate_info, &command_buff);
+    vkBeginCommandBuffer(command_buff, &begin_info);
+
+    return command_buff;
+}
+
+void VulkanManager::end_single_time_commands(VkCommandBuffer command_buff)
+{
+    VkQueue graphics_queue;
+    VkSubmitInfo submit_info{};
+
+    vkGetDeviceQueue(logical_device, get_graphics_family_index(), 0, &graphics_queue);
+
+    submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submit_info.commandBufferCount = 1;
+    submit_info.pCommandBuffers = &command_buff;
+
+    vkEndCommandBuffer(command_buff);
+    vkQueueSubmit(graphics_queue, 1, &submit_info, VK_NULL_HANDLE);
+    vkQueueWaitIdle(graphics_queue);
+
+    vkFreeCommandBuffers(logical_device, command_pool, 1, &command_buff);
 }
 
 void VulkanManager::add_command_buffers()
@@ -1152,6 +1306,9 @@ void VulkanManager::process()
 void VulkanManager::cleanup()
 {
     remove_swap_chain();
+
+    vkDestroyImage(logical_device, texture_image, nullptr);
+    vkFreeMemory(logical_device, texture_image_memory, nullptr);
 
     for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
     {
